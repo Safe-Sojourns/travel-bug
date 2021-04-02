7d24ccd487c9c0df6e6870a1cbb0fc4225f85237
gabinochacon8 <gabino.chacon8@gmail.com>
2021-04-02 16:32:32 -0700

    Initital commit

---
+++ travel_bug/app/screens/WelcomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+
+function WelcomeScreen(props) {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello From Gabe</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    textAlign: 'center'
+  }
+})
+
+export default WelcomeScreen;
