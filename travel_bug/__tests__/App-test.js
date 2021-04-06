0b503db6ed2b252c155635a1adb27b26771979cb
EricMHernandez <erich7912@gmail.com>
2021-04-06 17:17:22 -0400

    added more styling

--- travel_bug/__tests__/App-test.js
+++ travel_bug/__tests__/App-test.js
@@ -2,13 +2,22 @@
  * @format
  */
 
+// import 'react-native';
+// import React from 'react';
+// import App from '../App';
+
+// // Note: test renderer must be required after react-native.
+// import renderer from 'react-test-renderer';
+
+// it('renders correctly', () => {
+//   renderer.create(<App />);
+// });
+
 import 'react-native';
 import React from 'react';
-import App from '../App';
-
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+import App from '../app/App.js';
+test('renders correctly', () => {
+  const tree = renderer.create(<App />);
+  expect(tree).toMatchSnapshot();
 });
